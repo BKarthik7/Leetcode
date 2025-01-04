03bf972df9e54550ac43ada52eceb7562409c4bb
B Karthik <bangikarthik7@gmail.com>
2025-01-04 08:53:16 +0530

    2270

---
+++ 2270.number-of-ways-to-split-array.cpp
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=2270 lang=cpp
+ *
+ * [2270] Number of Ways to Split Array
+ */
+
+// @lc code=start
+
+#include <numeric>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    static int waysToSplitArray(vector<int>& nums) {
+        const int n=nums.size();
+        long long sum=accumulate(nums.begin(), nums.end(), 0LL);
+        long long acc=0LL;
+        int cnt=0;
+        for(int i=0; i<n-1; i++){
+            acc+=nums[i];
+            cnt+=(2*acc>=sum);
+        }
+        return cnt;
+    }
+};
+// @lc code=end
+
