5f22680e0eb9179f3c1ab0a6ebe581babce9f9ab
B Karthik <115967163+BKarthik7@users.noreply.github.com>
2025-01-21 19:44:14 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (48.04%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -4,11 +4,11 @@ public:
         if(s.length() != t.length()){
             return false;
         }
-        unordered_map<int, int> mp;
+        vector<int> mp(26, 0);
 
-        for(char c: s) mp[c]++;
-        for(char c: t) mp[c]--;
-        for(auto c: mp) if(c.second != 0) return false;
+        for(char c: s) mp[c - 'a']++;
+        for(char c: t) mp[c - 'a']--;
+        for(int c: mp) if(c != 0) return false;
         return true;
     }
 };
