952736c1f876a4880cbd6381f06f794a3dc72e9a
B Karthik <115967163+BKarthik7@users.noreply.github.com>
2025-02-10 19:14:19 +0530

    Time: 1 ms (25.26%) | Memory: 9.1 MB (41.76%) - LeetSync

--- 3447-clear-digits/clear-digits.cpp
+++ 3447-clear-digits/clear-digits.cpp
@@ -1,14 +1,22 @@
+#include <stack>
+#include <string>
+
 class Solution {
 public:
     string clearDigits(string s) {
-        string stk;
-        for (char c : s) {
-            if (isdigit(c)) {
-                if (!stk.empty()) stk.pop_back();
-            } else {
-                stk.push_back(c);
+        stack<char> stk;
+        for(int i = 0; i < s.length(); i++){
+            if(s[i] >= '0' && s[i] <='9'){
+                if(!stk.empty()) stk.pop();
+            } else{
+                stk.push(s[i]);
             }
         }
-        return stk;
+        string ans = "";
+        while(!stk.empty()){
+            ans = stk.top() + ans;
+            stk.pop();
+        }
+        return ans;
     }
 };
